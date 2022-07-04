a5a288a66db4202e8df9484704ad37b84eb7043e
Nuri1977 <nurilacka@gmail.com>
2022-07-04 12:27:42 +0200

    Display images in home

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import './Navigation.css';
 
 const Navigation = () => (
-  <div className=" navbar flex justify-between align-center">
-    <div className="text-center equal-length" />
-    <div className="text-center equal-length">
-      <div className="logo">
-        valstock
-      </div>
+  <menu className="navbar">
+    <div className="flex justify-between align-center conatiner">
+      <div className="text-center equal-length" />
+      <div className="text-center equal-length">
+        <div className="logo">
+          valstock
+        </div>
 
+      </div>
+      <div className="flex justify-center equal-length">
+        <div className="album">My albums</div>
+      </div>
     </div>
-    <div className="flex justify-center equal-length">
-      <div className="album">My albums</div>
-    </div>
-  </div>
+  </menu>
+
 );
 
 export default Navigation;
--- src/components/Picsum.jsx
+++ src/components/Picsum.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './Picsum.css';
 
 function Picsum() {
   const picsums = useSelector((state) => state.picsumReducer);
   console.log(picsums);
   return (
-    <div>Picsum</div>
+    <section className="picsum">
+      <div className="container border">
+        <ul className="pixum-grid">
+          {picsums.map((picsum) => (
+            <li key={picsum.id} id={picsum.id}>
+              <img src={picsum.download_url} alt="" className="picsum-image" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 }
 
